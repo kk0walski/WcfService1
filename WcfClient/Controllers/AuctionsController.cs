40e4dd3a14a02807f71596de04fa85849472d893
karol <karol@DESKTOP-A5DT1UI>
2017-06-12 03:27:32 +0200

    Adding auction controller

---
+++ WcfClient/Controllers/AuctionsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WcfClient.ServiceReference1;
+
+namespace WcfClient.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        // GET: Auctions
+        public ActionResult Index()
+        {
+            ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+            return View(obj.getAll());
+        }
+
+        // GET: Auctions/Details/5
+        public ActionResult Details(int id)
+        {
+            ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+            return View(obj.getById(id.ToString()));
+        }
+
+        // GET: Auctions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auctions/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ID,Finished,Name,StartPrice,EndDate")]Auction auction)
+        {
+            try
+            {
+                ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+                obj.AddAutcion(auction);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auctions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+            Auction aukcja = obj.getById(id.ToString());
+            if(aukcja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aukcja);
+        }
+
+        // POST: Auctions/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, [Bind(Include = "ID,Finished,Name,StartPrice,EndDate")]Auction auction)
+        {
+            try
+            {
+                ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+                obj.UpdateAuction(auction, id.ToString());
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auctions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+            Auction aukcja = obj.getById(id.ToString());
+            if (aukcja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aukcja);
+        }
+
+        // POST: Auctions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+                obj.deleteAuction(id.ToString());
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WcfClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WcfClient.Startup))]
+namespace WcfClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
